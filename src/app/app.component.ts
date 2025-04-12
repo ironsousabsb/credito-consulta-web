0a0f2b4cef3c1c8e79d7a4e187ae8bb0fc93a511
Iron Sousa <ironsousa@hotmail.com>
2025-04-11 22:11:05 -0300

    feat: Adicionar formulário de listagem de crédito com filtro por NFS-e e por número

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import credito from './credito.json';
+import { ListarCreditosComponent } from "./componentes/listar-creditos/listar-creditos.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { ContainerComponent } from "./componentes/container/container.component";
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { ListarCreditosComponent } from './componentes/listar-creditos/listar-creditos.component';
+import { FormsModule } from '@angular/forms';
+
+interface Credito {
+  numeroCredito: string;
+  numeroNfse: string;
+  dataConstituicao: string;
+  valorIssqn: number;
+  tipoCredito: string;
+  simplesNacional: string;
+  aliquota: number;
+  valorFaturado: number;
+  valorDeducao: number;
+  baseCalculo: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [
-    ContainerComponent, 
-    ContainerComponent, 
-    CabecalhoComponent,
-    ListarCreditosComponent
-  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    ListarCreditosComponent,
+    CabecalhoComponent,
+    ContainerComponent,
+    FormsModule
+  ],
+  standalone: true,
 })
 export class AppComponent {
   title = 'CrediConsultaWeb';
+  creditos: Credito[] = credito;
+  filtroBusca: string = '';
+
+  get creditosFiltrados(): Credito[] {
+    if (!this.filtroBusca.trim()) {
+      return this.creditos;
+    }
+
+    const termo = this.filtroBusca.toLowerCase();
+
+    return this.creditos.filter(credito =>
+      credito.numeroNfse.toLowerCase().includes(termo) ||
+      credito.numeroCredito.toLowerCase().includes(termo)
+    );
+  }
 }
